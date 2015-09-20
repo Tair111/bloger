515414f5d2e3c817f1231020fdb97b6012e68b25
Tair111 <obra-aleksandr@yandex.ru>
2015-09-20 10:18:56 +0300

    MVC

--- article.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-include_once __DIR__ . '/boot.php';
-
-$Controller = new NewsController();
-$Controller->action('one');
--- edit.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//header("Content-Type: text/html; charset=utf-8");
-
-include_once __DIR__ . '/boot.php';
-
-
-$Controller = new NewsController();
-$Controller->action('edit');
